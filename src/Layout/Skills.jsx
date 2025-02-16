b83463677924307b9d178173b479a796e642ca99
ivaraz <rifamaulana243@gmail.com>
2025-02-16 13:01:19 +0700

    a

--- src/Layout/Skills.jsx
+++ src/Layout/Skills.jsx
@@ -14,7 +14,7 @@ export default function Skills() {
       >
         Stack
       </h1>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
+      <div className="grid grid-cols-3 gap-5">
         {dataSkills.map((data, index) => (
           <CardSkills key={index} {...data} />
         ))}
--- src/data/DataSkills.jsx
+++ src/data/DataSkills.jsx
@@ -32,51 +32,51 @@ const dataSkills = [
     animate: "zoom-in",
     duration: 2000,
   },
-  {
-    logos:
-      "https://img.icons8.com/?size=100&id=fAMVO_fuoOuC&format=png&color=000000",
-    title: "PHP",
-    animate: "zoom-in",
-    duration: 2000,
-  },
-  {
-    logos:
-      "https://img.icons8.com/?size=100&id=9nLaR5KFGjN0&format=png&color=000000",
-    title: "MySQL",
-    animate: "zoom-in",
-    duration: 2000,
-  },
-  {
-    logos:
-      "https://img.icons8.com/?size=100&id=hUvxmdu7Rloj&format=png&color=000000",
-    title: "Laravel",
-    animate: "zoom-in",
-    duration: 2000,
-  },
+  // {
+  //   logos:
+  //     "https://img.icons8.com/?size=100&id=fAMVO_fuoOuC&format=png&color=000000",
+  //   title: "PHP",
+  //   animate: "zoom-in",
+  //   duration: 2000,
+  // },
+  // {
+  //   logos:
+  //     "https://img.icons8.com/?size=100&id=9nLaR5KFGjN0&format=png&color=000000",
+  //   title: "MySQL",
+  //   animate: "zoom-in",
+  //   duration: 2000,
+  // },
+  // {
+  //   logos:
+  //     "https://img.icons8.com/?size=100&id=hUvxmdu7Rloj&format=png&color=000000",
+  //   title: "Laravel",
+  //   animate: "zoom-in",
+  //   duration: 2000,
+  // },
   {
     logos: "https://img.icons8.com/?size=100&id=123603&format=png&color=000000",
     title: "React",
     animate: "zoom-in",
     duration: 2000,
   },
-  {
-    logos: "https://img.icons8.com/?size=100&id=40669&format=png&color=000000",
-    title: "C++",
-    animate: "zoom-in",
-    duration: 2000,
-  },
-  {
-    logos: "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",
-    title: "Python",
-    animate: "zoom-in",
-    duration: 2000,
-  },
-  {
-    logos: "https://img.icons8.com/?size=100&id=13679&format=png&color=000000",
-    title: "Java",
-    animate: "zoom-in",
-    duration: 2000,
-  },
+  // {
+  //   logos: "https://img.icons8.com/?size=100&id=40669&format=png&color=000000",
+  //   title: "C++",
+  //   animate: "zoom-in",
+  //   duration: 2000,
+  // },
+  // {
+  //   logos: "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",
+  //   title: "Python",
+  //   animate: "zoom-in",
+  //   duration: 2000,
+  // },
+  // {
+  //   logos: "https://img.icons8.com/?size=100&id=13679&format=png&color=000000",
+  //   title: "Java",
+  //   animate: "zoom-in",
+  //   duration: 2000,
+  // },
 ];
 
 export default dataSkills;
