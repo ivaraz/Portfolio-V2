701608f7a54dd0c442c2803b71bc1c1a1a4eb882
ivaraz <rifamaulana243@gmail.com>
2025-03-04 01:38:01 +0700

    add lanyard

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,19 +1,18 @@
 import Button from "../components/Button";
 import Rifa from "/assets/photos/ivar.jpeg";
+import Lanyard from "../components/Lanyard/Lanyard";
 
 export default function Home() {
   return (
     <div
       id="home"
-      className="lg:flex lg:flex-row-reverse lg:justify-around flex flex-col justify-center items-center gap-11 max-w-full h-screen bg-white"
+      className="lg:flex lg:flex-row-reverse lg:justify-around flex flex-col justify-center items-center  max-w-full h-screen bg-white"
     >
-      <div
-        data-aos="fade-right"
-        className="lg:w-4/12 w-[15rem] h-[15rem] max-w-screen-sm bg-[50%_30%] rounded-sm bg-cover"
-        style={{ backgroundImage: `url(${Rifa})` }}
-      ></div>
+      <div className="w-full h-screen max-w-screen-sm bg-[50%_30%] rounded-sm bg-cover">
+        <Lanyard fov={25} position={[0, 0, 13]} gravity={[0, -40, 0]} />
+      </div>
       <div data-aos="fade-left" className="flex flex-col lg:gap-5 gap-3">
-        <h1 className="lg:text-5xl text-3xl font-semibold flex flex-col">
+        <h1 className="lg:text-5xl text-4xl font-semibold flex flex-col">
           <span>
             Muhammad <span className="text-red-600">Rifa</span>
           </span>
