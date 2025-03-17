9ca31ab1146ac301837caccbb3cb680345843efe
ivaraz <rifamaulana243@gmail.com>
2025-03-18 00:31:49 +0700

    fix about

--- src/Layout/About.tsx
+++ src/Layout/About.tsx
@@ -35,19 +35,19 @@ export default function About() {
           className="lg:w-[50rem] min-w-fit max-w-screen-sm"
         >
           <div className="flex lg:justify-start">
-            <h1 className="lg:text-4xl font-bai font-semibold mb-3 text-start">
+            <h1 className="lg:text-4xl mx-5 lg:mx-0 font-bai font-semibold mb-3 text-start">
               About Me
             </h1>
             <span className="w-[15%] self-center ml-3 rounded-lg h-[0.2rem] bg-black"></span>
           </div>
-          <div className="lg:text-xl gap-5 grid lg:text-md lg:mx-0 w-[20rem] text-sm lg:w-[45rem]">
-            <p>
+          <div className="gap-5 grid lg:text-lg lg:mx-0 w-full text-sm lg:w-[45rem]">
+            <p className="text-justify mx-5 lg:mx-0">
               Hello everyone!! My fullname is Muhammad Rifa Maulana Aziz, and I
               was born on 23 April 2005 ({age} years old). I'm an Informatics
-              Student at Suryakancana University and have expertise in Front End
+              Student at Suryakancana University and have interesting on Front End
               development and computer networking.
             </p>
-            <div className="flex gap-5">
+            <div className="flex lg:gap-5 gap-3 justify-center items-center">
             {DataSos.map((item, index) => (
                <Details key={index} logo={item.logo} sosmed={item.sosmed} link={item.link} />
             ))}
